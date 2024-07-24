5b6bfc2c0893dd8f7b652f8a838cc83bf37ecf5d
Alessandra Ribeiro Guimaraes <alessandra1ribeiroguimaraes@gmail.com>
2024-07-24 10:50:57 -0300

    Criação das pastas por exercicio com seus respectivos arquivos

---
+++ exercicio_1/exerc_1.py
@@ -0,0 +1,4 @@
+'''
+exercício 1 do Desafio_1 - Faça um Programa que peça dois números, realize as principais
+operações soma, subtração, multiplicação, divisão.
+'''
---
+++ exercicio_3/exerc_3.py
@@ -0,0 +1,4 @@
+'''
+exercício 3 do Desafio_1 -Faça um Programa que peça a quantidade de quilômetros, transforme
+em metros, centímetros e milímetros.
+'''
